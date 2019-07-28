f6452dfa4c6a703e8ca9876da3541b5115697bf3
Connor Perez <connorperez@gmail.com>
2019-07-28 11:24:05 -0500

    npm installs react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import NavTabs from "./components/NavTabs";
 
